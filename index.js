fc1ed50f714fe116467a4df8945f4d0a539cbef5
Dana <estdanar@gmail.com>
2020-09-01 15:24:49 +0300

    local storage edit

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-conutertodo = 0;
-conuterdone = 0;
-container = document.querySelector(".task_container");
+let conutertodo = 0;
+let conuterdone = 0;
+let taskDeleted;
+let container = document.querySelector(".task_container");
 function emptyText() {
   document.getElementById('search').value = '';
 }
-tasks = []
+let tasks = JSON.parse(window.localStorage.getItem('tasks')) ?? [];
+showTasks(tasks, tasks.length)
 function iconsClick(event) {
   if (event.target.matches('.fa-trash')) {
-    showConfirmDelete()
     taskDeleted = event.target;
+    showConfirmDelete()
   } else if (event.target.matches('.fa-check-circle')) {
     changeStatus(event.target)
   }
@@ -17,114 +19,116 @@ function iconsClick(event) {
 
 container.addEventListener('click', iconsClick);
 function deleteTask() {
-  deletedSection = taskDeleted.closest('section');
+  let deletedSection = taskDeleted.closest('section');
   deleteFromArray(deletedSection)
-  container.innerHTML="";
+  container.innerHTML = "";
   hideConfirmDelete()
-  showTasks(tasks,tasks.length);
+  showTasks(tasks, tasks.length);
 }
 function changeStatus(ev) {
-  targetSection = ev.closest('section');
-  indexOfSection = tasks.findIndex(task => task.task_name === targetSection.className);
+  let targetSection = ev.closest('section');
+  let indexOfSection = tasks.findIndex(task => task.taskName === targetSection.className);//
   if (targetSection.dataset.status === "toDo") {
     targetSection.setAttribute("data-status", "Done");
     conutertodo--;
     conuterdone++;
+    window.localStorage.setItem('conutertodo', JSON.stringify(conutertodo));
+    window.localStorage.setItem('conuterdone', JSON.stringify(conuterdone));
     displayCounter()
-    const newTask = { ...tasks[indexOfSection], data_status: "Done" };
+    let newTask = { ...tasks[indexOfSection], dataStatus: "Done" };
     tasks.splice(indexOfSection, 1, newTask);
-    console.log(newTask)
-
-    console.log(tasks)
   } else {
     targetSection.setAttribute("data-status", "toDo");
-    tasks[indexOfSection].data_status == "toDo";
-    console.log(tasks[indexOfSection].data_status)
+    tasks[indexOfSection].dataStatus == "toDo";
     conuterdone--;
     conutertodo++;
+    window.localStorage.setItem('conutertodo', JSON.stringify(conutertodo));
+    window.localStorage.setItem('conuterdone', JSON.stringify(conuterdone));
     displayCounter()
   }
 }
 function creatTaskName(taskName) {
-  var wrapper_task = document.createElement("div");
-  wrapper_task.className = "task_section";
-  var task_name = document.createElement("p");
-  task_name.className = "task_name";
-  task_name.innerHTML = taskName;
-  var deleteIcon = document.createElement("i");
+  let wrapperTask = document.createElement("div");
+  wrapperTask.className = "task_section";
+  taskNameP = document.createElement("p");
+  taskNameP.className = "taskNameP";
+  taskNameP.innerHTML = taskName;
+  console.log(taskName)
+  let deleteIcon = document.createElement("i");
   deleteIcon.className = "fa fa-trash fa-2x";
-  wrapper_task.appendChild(task_name);
-  wrapper_task.appendChild(deleteIcon);
-  return wrapper_task;
+  wrapperTask.appendChild(taskNameP);
+  wrapperTask.appendChild(deleteIcon);
+  return wrapperTask;
 }
 function createAssigneeName(assigneeName) {
-  var wrapper_assignee = document.createElement("div");
-  wrapper_assignee.className = "assignee_section";
-  var assignee_name = document.createElement("p");
-  assignee_name.className = "assignee_value";
-  assignee_name.innerHTML = assigneeName;
-  var checkIcon = document.createElement("i");
+  let wrapperAssignee = document.createElement("div");
+  wrapperAssignee.className = "assignee_section";
+  assigneeNameP = document.createElement("p");
+  assigneeNameP.className = "assignee_value";
+  assigneeNameP.innerHTML = assigneeName;
+  let checkIcon = document.createElement("i");
   checkIcon.className = "fa fa-check-circle fa-2x";
-  wrapper_assignee.appendChild(assignee_name);
-  wrapper_assignee.appendChild(checkIcon);
-  return wrapper_assignee;
+  wrapperAssignee.appendChild(assigneeNameP);
+  wrapperAssignee.appendChild(checkIcon);
+  return wrapperAssignee;
 }
 function showTasks(arr, len) {
-  for (var i = 0; i < len; i++) {
-    var section = document.createElement("section");
-    section.className = arr[i].task_name;
+  for (let i = 0; i < len; i++) {
+    let section = document.createElement("section");
+    section.className = arr[i].taskName;
     section.setAttribute("data-status", "toDo");
-    var taskName = arr[i].task_name;
-    var assigneeName = arr[i].Assignee_name;
-    var task = creatTaskName(taskName);
-    var assignee = createAssigneeName(assigneeName);
+    let taskName = arr[i].taskName;
+    let assigneeName = arr[i].assigneeName;
+    let task = creatTaskName(taskName);
+    let assignee = createAssigneeName(assigneeName);
     section.appendChild(task);
     section.appendChild(assignee);
     container.prepend(section);
+    displayCounter()
   }
 }
 function showResult(arr, len) {
-  conuterdone = 0;
   conutertodo = 0;
-  for (var i = 0; i < len; i++) {
-    var section = document.createElement("section");
-    section.className = arr[i].task_name;
-    section.setAttribute("data-status", arr[i].data_status);
+  conuterdone = 0;
+  for (let i = 0; i < len; i++) {
+    let section = document.createElement("section");
+    section.className = arr[i].taskName;
+    section.setAttribute("data-status", arr[i].dataStatus);
     secAttribute = section.getAttribute("data-status");
     if (secAttribute === "Done") {
       conuterdone++;
     } else {
       conutertodo++;
     }
-    var taskName = arr[i].task_name;
-    var assigneeName = arr[i].Assignee_name;
-    var task = creatTaskName(taskName);
-    var assignee = createAssigneeName(assigneeName);
+    window.localStorage.setItem('conutertodo', JSON.stringify(conutertodo));
+    window.localStorage.setItem('conuterdone', JSON.stringify(conuterdone));
+    let taskName = arr[i].taskName;
+    let assigneeName = arr[i].assigneeName;
+    let task = creatTaskName(taskName);
+    let assignee = createAssigneeName(assigneeName);
     section.appendChild(task);
     section.appendChild(assignee);
     container.appendChild(section);
   }
   document.getElementsByTagName('footer').innerHTML = "";
-  console.log(conutertodo)
   displayCounter();
 }
 
 function addTask() {
-  var taskName = document.getElementById('task_name').value;
-  var assigneeName = document.querySelector(".assignee_text").value;
+  let taskName = document.getElementById('task_name').value;
+  let assigneeName = document.querySelector(".assignee_text").value;
   if (taskName === "" && assigneeName == "") return
   const task = {
-    "task_name": `${taskName}`,
-    "Assignee_name": `${assigneeName}`,
-    "data_status": "toDo"
+    "taskName": `${taskName}`,
+    "assigneeName": `${assigneeName}`,
+    "dataStatus": "toDo"
   }
   tasks.unshift(task);
+  window.localStorage.setItem('tasks', JSON.stringify(tasks));
   conutertodo++;
-  console.log(conutertodo)
-  console.log(conuterdone)
+  window.localStorage.setItem('conutertodo', JSON.stringify(conutertodo));
   showTasks(tasks, 1);
   displayCounter()
-  console.log(tasks)
   document.getElementById('task_name').value = '';
   document.querySelector(".assignee_text").value = '';
 }
@@ -135,25 +139,23 @@ function hideConfirmDelete() {
   document.getElementById("confirm_delete").style.opacity = 0;
 }
 function deleteFromArray(deletedSection) {
-  const deletedtask = tasks.filter(task => task.task_name === deletedSection.className);
+  const deletedtask = tasks.filter(task => task.taskName === deletedSection.className);
   if (deletedSection.getAttribute('data-status') === "Done") {
-    conuterdone--;
+    window.localStorage.setItem('conuterdone', JSON.stringify(--conuterdone));
   } else {
-    conutertodo--;
+    window.localStorage.setItem('conutertodo', JSON.stringify(--conutertodo));
   }
-
-  console.log(deletedtask)
   displayCounter()
-  tasks = tasks.filter(task => task.task_name != deletedSection.className);
-  console.log(tasks)
+  tasks = tasks.filter(task => task.taskName != deletedSection.className);
+  window.localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 function displayCounter() {
-  document.getElementById("todo").innerHTML = conutertodo;
-  document.getElementById("done").innerHTML = conuterdone;
+  document.getElementById("todo").innerHTML = JSON.parse(window.localStorage.getItem('conutertodo'));
+  document.getElementById("done").innerHTML = JSON.parse(window.localStorage.getItem('conuterdone'));
 }
 function search() {
   taskName = document.getElementById('search').value;
-  let resultArray = tasks.filter(task => task.task_name.includes(taskName));
+  let resultArray = tasks.filter(task => task.taskName.includes(taskName));
   container.innerHTML = "";
   showResult(resultArray, resultArray.length)
 
